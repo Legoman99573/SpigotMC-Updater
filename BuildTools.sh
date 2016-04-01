75e48655ad162db3c6d16c53607ee9ebb8ed9690
Nato Boram <NatoBoram@users.noreply.github.com>
2016-04-01 13:53:33 -0400

    Update BuildTools.sh

--- BuildTools.sh
+++ BuildTools.sh
@@ -6,6 +6,7 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 
 You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/."
 clear
+curl -o License.txt https://raw.githubusercontent.com/Legoman99573/SpigotMC-Updater/master/License.md
 curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastBuild/artifact/target/BuildTools.jar
 java -jar BuildTools.jar --rev latest
 rm BuildTools.jar
